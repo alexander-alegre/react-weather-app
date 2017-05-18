81ccbbbcdaa45562d06a577dad7738383989ea5c
= <alexander.alegre@outlook.com>
2017-05-17 18:50:05 -0700

    add foundation and jquery to project

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ let express = require('express');
 let app = express();
 // public folder
 app.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-        next();
-    } else {
+    if (req.headers['x-forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();   
     }
 });
 app.use(express.static('public'));
@@ -15,3 +15,6 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}/`);
 });
+
+
+// npm i foundation-sites@6.2.0 --save-exact --save-dev
