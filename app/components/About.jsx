d6e6dd16dff5232966c402bfc42e55e7f6e3aef0
= <alexander.alegre@outlook.com>
2017-05-17 19:42:43 -0700

    style about page and link to resources used

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,7 +13,13 @@ let React = require('react');
 let About = (props) => {
     return (
       <div>
-        <h3>about</h3>
+        <h1 className="text-center">About</h1>
+        <p>This is a weather application built on react. I have built this for The Complete React Developer Course</p>
+        <p>Here are some of the tools i used</p>
+        <ul>
+          <li><a href="https://facebook..github.io/react">React</a> - JavaScript framework used.</li>
+          <li><a href="http://openweathermao.org">Open Weather Map</a> - I used this to get the weather data.</li>
+        </ul>
       </div>
     )
 }
--- public/bundle.js
+++ public/bundle.js
@@ -26299,7 +26299,7 @@
 /* 246 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
@@ -26315,12 +26315,46 @@
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'div',
+	    "div",
 	    null,
 	    React.createElement(
-	      'h3',
+	      "h1",
+	      { className: "text-center" },
+	      "About"
+	    ),
+	    React.createElement(
+	      "p",
 	      null,
-	      'about'
+	      "This is a weather application built on react. I have built this for The Complete React Developer Course"
+	    ),
+	    React.createElement(
+	      "p",
+	      null,
+	      "Here are some of the tools i used"
+	    ),
+	    React.createElement(
+	      "ul",
+	      null,
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "https://facebook..github.io/react" },
+	          "React"
+	        ),
+	        " - JavaScript framework used."
+	      ),
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "http://openweathermao.org" },
+	          "Open Weather Map"
+	        ),
+	        " - I used this to get the weather data."
+	      )
 	    )
 	  );
 	};
